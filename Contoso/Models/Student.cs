a9690445bb65e166c70656b9ffa63129996fc6e8
Henry Lai <bzlai@aol.com>
2017-01-10 15:34:35 -0800

    Initial Commit

---
+++ Contoso/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Contoso.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public String LastName { get; set; }
+        public String FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set;}
+
+    }
+}
