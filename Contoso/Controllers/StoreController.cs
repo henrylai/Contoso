dd09a35ca360e774de23c61ef6b45d60db0fb076
Henry Lai <bzlai@aol.com>
2017-01-12 15:15:17 -0800

    Added Store Controller.

---
+++ Contoso/Controllers/StoreController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Contoso.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "Hi from the Store Controller --> Index page";
+        }
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+            return message;
+        }
+        public string Genre()
+        {
+            return "Hi from the Store Controller --> Genre page";
+        }
+    }
+}
